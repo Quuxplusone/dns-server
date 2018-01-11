39f129bcef9daf2070b3eef8affb4e181008b849
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2018-01-11 14:50:42 -0800

    Turn RCode and Opcode into enum-like classes, not plain enums. NFC.
    This lets me attach a `repr()` method to variables of these types,
    so that for example I can write
    
        std::cout << RCode(3).repr() << std::endl;
    
    instead of something like `rcode_repr(3)` or `repr(RCode(3))`.
    The latter would actually be great, in my Python-flavored worldview,
    but it is basically impossible to get that syntax in C++ because
    if you are inside a function or method named `repr`, the one out in
    the outer namespace will be shadowed (unless you `using ::repr`
    explicitly, but that's horribly ugly and omission-prone).

--- src/include/opcode.h
+++ src/include/opcode.h
@@ -1,13 +1,35 @@
 #pragma once
 
 #include <inttypes.h>
+#include <string>
 
 namespace dns {
 
-enum Opcode : uint8_t {
-    QUERY = 0,
-    IQUERY = 1,
-    STATUS = 2,
+class Opcode {
+public:
+    enum detail : uint8_t {
+        QUERY = 0,
+        IQUERY = 1,
+        STATUS = 2,
+    };
+
+    explicit constexpr Opcode(int x) : m_value(x) {}
+    constexpr Opcode(enum Opcode::detail x) : m_value(int(x)) {}
+    explicit constexpr operator int() const { return m_value; }
+    constexpr bool operator==(Opcode rhs) const { return m_value == rhs.m_value; }
+    constexpr bool operator!=(Opcode rhs) const { return m_value != rhs.m_value; }
+
+    std::string repr() const {
+        switch (m_value) {
+            case QUERY: return "QUERY";
+            case IQUERY: return "IQUERY";
+            case STATUS: return "STATUS";
+            default: return std::to_string(int(m_value));
+        }
+    }
+
+private:
+    uint8_t m_value;
 };
 
 } // namespace dns
--- src/include/rcode.h
+++ src/include/rcode.h
@@ -1,16 +1,41 @@
 #pragma once
 
 #include <inttypes.h>
+#include <string>
 
 namespace dns {
 
-enum RCode : uint8_t {
-    NOERROR = 0,
-    FORMERR = 1,
-    SERVFAIL = 2,
-    NXDOMAIN = 3,
-    NOTIMP = 4,
-    REFUSED = 5,
+class RCode {
+public:
+    enum detail : uint8_t {
+        NOERROR = 0,
+        FORMERR = 1,
+        SERVFAIL = 2,
+        NXDOMAIN = 3,
+        NOTIMP = 4,
+        REFUSED = 5,
+    };
+
+    explicit constexpr RCode(int x) : m_value(x) {}
+    constexpr RCode(enum RCode::detail x) : m_value(int(x)) {}
+    explicit constexpr operator int() const { return m_value; }
+    constexpr bool operator==(RCode rhs) const { return m_value == rhs.m_value; }
+    constexpr bool operator!=(RCode rhs) const { return m_value != rhs.m_value; }
+
+    std::string repr() const {
+        switch (m_value) {
+            case NOERROR: return "NOERROR";
+            case FORMERR: return "FORMERR";
+            case SERVFAIL: return "SERVFAIL";
+            case NXDOMAIN: return "NXDOMAIN";
+            case NOTIMP: return "NOTIMP";
+            case REFUSED: return "REFUSED";
+            default: return std::to_string(int(m_value));
+        }
+    }
+
+private:
+    uint8_t m_value;
 };
 
 } // namespace dns
